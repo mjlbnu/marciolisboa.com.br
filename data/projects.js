8132c19ee5b6bc2531dffdf01eed81ec3032ad0f
Marcio José Lisboa <mjlbnu@gmail.com>
2020-01-08 16:20:27 +0000

    adicionado link do deploy do proj. spots

--- data/projects.js
+++ data/projects.js
@@ -2,7 +2,7 @@ export default [
   {
     icon: '📝',
     title: 'Spots',
-    path: 'https://github.com/mjlbnu/spots',
+    path: 'https://thawing-savannah-09389.herokuapp.com/',
     source: 'https://github.com/mjlbnu/spots',
     description: 'Projeto produzido durante a semana Omnistack 9.0, com algumas features adicionais.',
   },
