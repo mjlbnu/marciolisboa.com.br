6548587a889b0eb47e083b30ae4e036a4a950362
Marcio Lisboa <mjlbnu@gmail.com>
2023-05-06 21:42:21 -0300

    New Google tag

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,51 +1,51 @@
 const config = {
-  siteTitle: 'Marcio José Lisboa',
-  siteTitleShort: 'Marcio José Lisboa',
-  siteTitleAlt: 'Marcio José Lisboa',
-  siteLogo: '/logos/logo-1024.png',
-  siteUrl: 'https://www.programadorbr.com.br',
-  repo: 'https://github.com/mjlbnu',
-  pathPrefix: '',
-  dateFromFormat: 'YYYY-MM-DD',
-  dateFormat: 'LL',
+  siteTitle: "Marcio José Lisboa",
+  siteTitleShort: "Marcio José Lisboa",
+  siteTitleAlt: "Marcio José Lisboa",
+  siteLogo: "/logos/logo-1024.png",
+  siteUrl: "https://www.programadorbr.com.br",
+  repo: "https://github.com/mjlbnu",
+  pathPrefix: "",
+  dateFromFormat: "YYYY-MM-DD",
+  dateFormat: "LL",
   siteDescription:
-    'Jardim  de Marcio José Lisboa. Artigos relacionados a desenvolvimento web, novas tecnologias, metodologias ágeis e carreira.',
-  siteRss: '/rss.xml',
-  googleAnalyticsID: 'UA-154535775-1',
-  postDefaultCategoryID: 'Tecnologia',
-  newsletter: '',
-  newsletterEmbed: '',
-  userName: 'Marcio',
-  userEmail: 'mjlbnu@gmail.com',
-  userTwitter: '',
+    "Jardim  de Marcio José Lisboa. Artigos relacionados a desenvolvimento web, novas tecnologias, metodologias ágeis e carreira.",
+  siteRss: "/rss.xml",
+  googleAnalyticsID: "G-G03CFN68HG",
+  postDefaultCategoryID: "Tecnologia",
+  newsletter: "",
+  newsletterEmbed: "",
+  userName: "Marcio",
+  userEmail: "mjlbnu@gmail.com",
+  userTwitter: "",
   menuLinks: [
     {
-      name: 'Sobre Mim',
-      link: '/sobre/',
+      name: "Sobre Mim",
+      link: "/sobre/",
     },
     {
-      name: 'Artigos',
-      link: '/blog/',
+      name: "Artigos",
+      link: "/blog/",
     },
   ],
-  themeColor: '#3F80FF', // Used for setting manifest and progress theme colors.
-  backgroundColor: '#ffffff',
+  themeColor: "#3F80FF", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#ffffff",
 };
 
 // Make sure pathPrefix is empty if not needed
-if (config.pathPrefix === '/') {
-  config.pathPrefix = '';
+if (config.pathPrefix === "/") {
+  config.pathPrefix = "";
 } else {
   // Make sure pathPrefix only contains the first forward slash
-  config.pathPrefix = `/${config.pathPrefix.replace(/^\/|\/$/g, '')}`;
+  config.pathPrefix = `/${config.pathPrefix.replace(/^\/|\/$/g, "")}`;
 }
 
 // Make sure siteUrl doesn't have an ending forward slash
-if (config.siteUrl.substr(-1) === '/')
+if (config.siteUrl.substr(-1) === "/")
   config.siteUrl = config.siteUrl.slice(0, -1);
 
 // Make sure siteRss has a starting forward slash
-if (config.siteRss && config.siteRss[0] !== '/')
+if (config.siteRss && config.siteRss[0] !== "/")
   config.siteRss = `/${config.siteRss}`;
 
 module.exports = config;
